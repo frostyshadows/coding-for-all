7bd7a72787dde92e4abb85158ed9e5d41ead41c2
Sherry Yuan <frostyyshadows@gmail.com>
2018-05-06 18:28:34 -0700

    change from art to article

--- link-to-json.py
+++ link-to-json.py
@@ -4,6 +4,6 @@ link = input("Enter URL\n")
 title = input("Enter title\n")
 level = input("Enter level (none OR some OR lots)\n")
 interest = input("Enter interest (android OR ios OR web)\n")
-type = input("Enter type (tut OR art OR vid)\n")
+type = input("Enter type (tutorial OR article OR video)\n")
 
 print("{\"link\": \"%s\", \"title\": \"%s\", \"options\": {\"level\": \"%s\", \"interest\": \"%s\", \"type\": \"%s\"}}" % (link, title, level, interest, type))
