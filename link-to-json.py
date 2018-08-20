2c06e3b91ab1192c12ccd30a289315d1a3bdc8f8
Russell Blickhan <rwblickhan@gmail.com>
2018-08-20 16:31:14 -0700

    Add input verfication and batch write to link-to-json script

--- link-to-json.py
+++ link-to-json.py
@@ -3,26 +3,55 @@
 
 import json
 
+valid_levels = [
+    'none',
+    'some',
+    'lots'
+]
+valid_interests = [
+    'android',
+    'ios',
+    'web'
+]
+valid_types = [
+    'tutorial',
+    'article',
+    'video'
+]
+
 links_file = open("links.json", "r")
 links = json.load(links_file)
 links_file.close()
 
-link = input("Enter URL:\n")
-title = input("Enter title:\n")
-level = input("Enter level (none OR some OR lots):\n")
-interest = input("Enter interest (android OR ios OR web):\n")
-type = input("Enter type (tutorial OR article OR video):\n")
+done = False
+while not done:
+    link = input("Enter URL:\n")
+    title = input("Enter title:\n")
+    level = input("Enter level {}:\n".format(valid_levels))
+    while not level in valid_levels:
+        level = input("Please input a valid level {}\n".format(valid_levels))
+    interest = input("Enter interest {}\n".format(valid_interests))
+    while not interest in valid_interests:
+        interest = input("Please input a valid interest {}\n".format(valid_interests))
+    type = input("Enter type {}\n".format(valid_types))
+    while not type in valid_types:
+        type = input("Please input a valid type {}\n".format(valid_types))
 
-added_info = {
-    'link': link,
-    'title': title,
-    'options': {
-        'level': level,
-        'interest': interest,
-        'type': type
-    }
-}
+    links.append({
+        'link': link,
+        'title': title,
+        'options': {
+            'level': level,
+            'interest': interest,
+            'type': type
+        }
+    })
 
-links.append(added_info)
+    done_str = ""
+    while done_str != 'y' and done_str != 'n':
+        done_str = input("Done? (y/n)")
+    if done_str == 'y':
+        done = True
+
 links_file = open("links.json", "w")
 json.dump(links, links_file, indent=4, separators=(',', ': '))
