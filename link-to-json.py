194433d00b6e3f3d10b74da5fbad1e547daf4bf5
Russell Blickhan <rwblickhan@gmail.com>
2018-08-20 16:06:35 -0700

    Implement writing json directly to file

--- link-to-json.py
+++ link-to-json.py
@@ -1,9 +1,28 @@
+#!/usr/bin/env python3
 # small command line tool for formatting info about a link into JSON
 
-link = input("Enter URL\n")
-title = input("Enter title\n")
-level = input("Enter level (none OR some OR lots)\n")
-interest = input("Enter interest (android OR ios OR web)\n")
-type = input("Enter type (tutorial OR article OR video)\n")
+import json
 
-print("{\"link\": \"%s\", \"title\": \"%s\", \"options\": {\"level\": \"%s\", \"interest\": \"%s\", \"type\": \"%s\"}}" % (link, title, level, interest, type))
+links_file = open("links.json", "r")
+links = json.load(links_file)
+links_file.close()
+
+link = input("Enter URL:\n")
+title = input("Enter title:\n")
+level = input("Enter level (none OR some OR lots):\n")
+interest = input("Enter interest (android OR ios OR web):\n")
+type = input("Enter type (tutorial OR article OR video):\n")
+
+added_info = {
+    'link': link,
+    'title': title,
+    'options': {
+        'level': level,
+        'interest': interest,
+        'type': type
+    }
+}
+
+links.append(added_info)
+links_file = open("links.json", "w")
+json.dump(links, links_file, indent=4, separators=(',', ': '))
